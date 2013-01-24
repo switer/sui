290141f85da88574c736494275c1a118deefa7ad
guanks <guankaishe@gmail.com>
2013-01-24 18:46:33 +0800

    popbox test

--- js/ui.popbox.js
+++ js/ui.popbox.js
@@ -2,26 +2,44 @@
 
   var conf = {
     "mask_class" : 'sui-popbox-mask',
-    'hide_class' : 'sui-disp-none'
+    'hide_class' : 'sui-disp-none',
+    'box_sel' : ".sui-popbox-box"
   }
-  // function _getMaxHeight (hArray) {
-  //   var max = hArray[0];
-  //   for (var i = 1, len = hArray.length; i < len; i--) {
-  //     if (hArray[i] > max) max = hArray[i]; 
-  //   }
-  //   return max;
-  // }
+  var _timer;
+  $.fn.checkPosition = function () {
+    console.log('check');
+    setInterval(function () {
+      $box.data('top') && $box.css('top', parseInt($box.data('top')) + document.body.scrollTop);
+    }, 100);
+  }
+  $.fn.trantision = function () {
+    $(this).css('webkitTransition', 'top 1s');
+  }
+  /**
+  * 显示弹框，并设置高度
+  */
   $.fn.showPopbox = function () {
-    var $popbox = $(this);
-    $popbox.removeClass(conf.hide_class);
+    var $popbox = $(this),
+        box = $popbox.find(conf.box_sel)[0];
+    $popbox.suiShow();
     var height =  document.body.scrollHeight;
     $popbox.height(height);
+    console.log($popbox.data('transition'));
+
+    if ( $popbox.data('position') === 'fixed' && $popbox.data('setPosition') !== 'fixed' ) {
+      $popbox.data('setPosition', 'fixed');
+      $popbox.fixed();
+    }
+    $popbox.data('transition') && box && (box.style['webkitTransition'] = 'top 1s');
   }
-  $.fn.hideSui = function () {
+  $.fn.suiHide = function () {
     var $sui = $(this);
     $sui.addClass(conf.hide_class);
   }
-  $.fn.center = function (e) {
+  $.fn.suiShow = function () {
+    $(this).removeClass(conf.hide_class);
+  }
+  $.fn.fixed = function (e) {
     var $popbox = $(this),
         $box = $popbox.find('.sui-popbox-box');
     function scrollHandler (event) {
@@ -30,7 +48,7 @@
       } 
       $box.css('top', parseInt($box.data('top')) + document.body.scrollTop);
     }
-    window.addEventListener('scroll',scrollHandler);
+    window.addEventListener('scroll', scrollHandler);
     scrollHandler();
   }
   $(document).on('click.sui-popbox>.sui-popbox-mask', function (e) {
@@ -40,7 +58,7 @@
     if ($parent.data('cancel')) {
       setTimeout(function () {
         //避免点击冒泡
-        $parent.hideSui();
+        $parent.suiHide();
       } ,100);
     }
     e.preventDefault();
