146598c70b0fcefe956535b7f4f86876e7fbba26
guanks <guankaishe@gmail.com>
2013-02-26 11:58:24 +0800

    move js to component

--- build.js
+++ build.js
@@ -6,26 +6,27 @@ var fs = require('fs')
 	, file
 	, cmbFilename = 'sui.js'
 	, miniFilename = 'sui.min.js'
-	, path = './js/';
+	, path = './js/'
+	, componentPath = './js/component/';
 
 for (var i = files.length - 1; i >= 0; i--) {
-	console.log('Build : ' + files[i]);
+	console.log('Build : ' + componentPath + files[i]);
 	comment = '\n/*' + files[i] + '*/\n'
-	file = fs.readFileSync(path + files[i],'UTF-8');
+	file = fs.readFileSync(componentPath + files[i],'UTF-8');
 	content += ( comment  + file );
 }
 fs.writeFileSync(path + cmbFilename, content, 'UTF-8');
-console.log('Combind File : ' + cmbFilename);
+console.log('\nCombind File : ' + path + cmbFilename);
 var result = UglifyJS.minify(path + cmbFilename);
-console.log('Minify File : ' + miniFilename);
+console.log('Minify File  : ' + path + miniFilename);
 fs.writeFileSync(path + miniFilename, result.code, 'UTF-8');
 
-var indexHtml = fs.readFileSync('index.html','UTF-8')
-	, layoutHtml = fs.readFileSync('layout.html','UTF-8')
-	, indexArray = indexHtml.split('<!--header-->')
-	, layoutHeader = layoutHtml.split('<!--header-->')[0];
+// var indexHtml = fs.readFileSync('index.html','UTF-8')
+// 	, layoutHtml = fs.readFileSync('layout.html','UTF-8')
+// 	, indexArray = indexHtml.split('<!--header-->')
+// 	, layoutHeader = layoutHtml.split('<!--header-->')[0];
 
-indexArray[0] = layoutHeader;
-console.log('Override html : ' + 'index.html');
-fs.writeFileSync('index_min.html', indexArray.join('<!--header-->'), 'UTF-8');
+// indexArray[0] = layoutHeader;
+// console.log('Override html : ' + 'index.html');
+// fs.writeFileSync('index.html', indexArray.join('<!--header-->'), 'UTF-8');
 	 
