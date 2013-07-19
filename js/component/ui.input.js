b3d96503757034664ed7fbb4e745cd55b5b51d37
switer <guankaishe@gmail.com>
2013-07-19 18:37:46 +0800

    update sui

--- js/component/ui.input.js
+++ js/component/ui.input.js
@@ -2,11 +2,14 @@
 
 !function ($) {
 
-  var conf = {};
+  var conf = {
+    "focus_class" : 'sui-focus',
+    "input_class" : 'sui-inp'
+  }
   /*Input public method*/
   $.fn.flushInp = _fulshInput;
 
-  $(document).on('click.sui-inp', _fulshInput);
+  // $(document).on('click.sui-inp', _fulshInput);
 
   /*flush Placeholder*/
   function _fulshInput (e) {
@@ -20,5 +23,32 @@
   function _isEmpty(val) {
     return (val === undefined || val === null || val.length === 0);
   }
+
+  /**
+  * Button Event
+  **/
+  $(document).on('click .sui-inp', function (e) {
+    var $tar = $(e.target);
+    if ( !$tar.hasClass(conf.input_class) ) return;
+    if ( $tar.data('focus') === 'true') {
+      //清空placeholder的value
+      _fulshInput.call(this,e)
+      //失去焦点
+      function _blur(e) {
+        //输入框的效果在父容器上
+        $tar.parent().removeClass(conf.focus_class)
+      }
+      // data-_blur用于标识已监听blur事件的私有标识
+      if ( $tar.data('_blur') !== 'true' ) {
+        //监听输入框的blur事件
+        $tar.on('blur', _blur)
+        $tar.data('_blur', 'true')
+      }
+      //输入框的效果在父容器上
+      $tar.parent().addClass(conf.focus_class)
+    }
+  });
+
+
 }(window.$);
 
