1db4fe36ef168d4e2c5e4f9bc1b89589d44a65ed
switer <guankaishe@gmail.com>
2013-08-15 15:35:15 +0800

    添加输入控件

--- js/component/ui.input.js
+++ js/component/ui.input.js
@@ -4,7 +4,8 @@
 
   var conf = {
     "focus_class" : 'sui-focus',
-    "input_class" : 'sui-inp'
+    "input_class" : 'sui-inp',
+    'input_control_length' : 10
   }
   /*Input public method*/
   $.fn.flushInp = _fulshInput;
@@ -23,7 +24,35 @@
   function _isEmpty(val) {
     return (val === undefined || val === null || val.length === 0);
   }
-
+  $.fn.initInp = function (value) {
+    $(this).data('init', value).setInp(value);
+  }
+  // 用于输入控件的设值
+  $.fn.setInp = function (value) {
+    var $this = $(this);
+    if ($this.data('ctrl')) {
+      //清除操作
+      if (value === '') {
+        $this.val('').data('value', '');
+      } else {
+        $this.val(Math.random().toString().slice(2, conf.input_control_length)).data('value', value);
+        $this.data('ismodify', 'false');
+      }
+    } else {
+      $this.val(value);
+    }
+  }
+  $.fn.getInp = function () {
+    var $this = $(this);
+    if ($this.data('ctrl')) {
+      return $this.data('value');
+    } else {
+      return $this.val();
+    }
+  }
+  $.fn.isModify = function () {
+    return ($(this).data('ismodify') === 'true');
+  }
   /**
   * Button Event
   **/
@@ -45,10 +74,55 @@
         $tar.on('blur', _blur)
         $tar.data('blur', 'true')
       }
+
       //输入框的效果在父容器上
       $tar.parent().addClass(conf.focus_class)
     }
+
+    /*是否为密码输入框控件*/
+    if ( $('body').data('ctrl') === 'true' && $tar.data('ctrl') === 'true' ) {
+      /*保存初始值*/
+      var oralValue = $tar.val();
+      //清空值，标识为已经聚焦，在DOM上保存初始值
+      $tar.data('oralvalue', oralValue);
+      //监听input事件
+      if ($tar.data('isattchinput') !== 'true') {
+        $tar.on('input', function () {
+          if ($tar.data('isclean') !== 'true') {
+            $tar.val('').data('isclean', 'true');
+          }
+        });
+        $tar.data('isattchinput', 'true');
+      }
+      //监听blur事件
+      if ($tar.data('isattchblur') !== 'true'){
+        $tar.on('blur', function () {
+          //获取初始值
+          var savedValue = $tar.data('oralvalue'),
+              inpValue = $tar.val();
+
+          //如果已经旧值已经被清除且输入值不为空时，往DOM保存输入值
+          if (($tar.data('isclean') === 'true') && inpValue) {
+            $tar.data('value', inpValue)
+          }
+          //如果保存值存在且输入不为空，认定为已更改
+          if (inpValue) {
+            $tar.data('ismodify', 'true');
+          }
+          //输入值为空时，还原清除前的值
+          else {
+            $tar.val(savedValue || '');
+          }
+          //初始还原已清除标识
+          $tar.data('isclean', 'false');
+        });
+        //标识为已监听blur事件
+        $tar.data('isattchblur', 'true');
+      }
+    }//end 输入控件
   });
+
+
 
 
 }(window.$);
